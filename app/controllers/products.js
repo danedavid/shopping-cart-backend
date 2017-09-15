662e33315c3c608042c47a459a8e2f4ecde8ce58
dane <dane@qburst.com>
2017-09-15 16:10:07 +0530

    Filters added

--- app/controllers/products.js
+++ app/controllers/products.js
@@ -1,11 +1,17 @@
 const productService = require('../services/products');
 
 module.exports = function (req, res) {
-  productService((data)=>{
+
+  const minPrice = (req.query.min_price)?req.query.min_price: null;
+  const maxPrice = (req.query.max_price)?req.query.max_price: null;
+  const limit = (req.query.limit)?req.query.limit: null;
+
+  productService( (err, data) => {
+    if(err) {}
     res.status(200).send({
       "status": 200,
       "total": data.length,
       "result": data
     });
-  });
+  }, minPrice, maxPrice, limit);
 };
