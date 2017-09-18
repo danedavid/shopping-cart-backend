5ce502388e97031506d8cd448afb0fd86b3756c9
dane <dane@qburst.com>
2017-09-18 11:26:30 +0530

    Error 422 for POST request

--- app/controllers/cart.js
+++ app/controllers/cart.js
@@ -2,6 +2,22 @@ const cartService = require('../services/cart');
 const _ = require('lodash');
 
 module.exports = function (req, res) {
+
+  let typeError = false;
+  if( !req.body.item ) typeError = true;
+  if( !req.body.item.hasOwnProperty('productID') ) typeError = true;
+  if( !req.body.item.hasOwnProperty('name') ) typeError = true;
+  if( !req.body.item.hasOwnProperty('price') ) typeError = true;
+  if( !req.body.item.hasOwnProperty('quantity') ) typeError = true;
+
+  if( typeError ) {
+    res.status(422).json({
+      "status": 422,
+      "error": "Invalid request body"
+    });
+    return;
+  }
+
   cartService(req.body.item, (err, data) => {
     if(err) {
       res.status(503).json({
@@ -10,6 +26,7 @@ module.exports = function (req, res) {
       });
       return;
     }
+
     res.status(200).json({
       status: 200,
       total: data.quantity,
