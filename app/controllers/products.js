b8d18354ca3eaa4b82ccabbbb9152c111a5727f0
dane <dane@qburst.com>
2017-09-18 10:24:34 +0530

    Error 422: invalid values for params

--- app/controllers/products.js
+++ app/controllers/products.js
@@ -6,8 +6,25 @@ module.exports = function (req, res) {
   const maxPrice = (req.query.max_price)?req.query.max_price: null;
   const limit = (req.query.limit)?req.query.limit: null;
 
+  let typeError = false;
+  if( isNaN(minPrice) || isNaN(minPrice) || isNaN(minPrice) ) typeError = true;
+
+  if(typeError) {
+    res.status(422).json({
+      "status": 422,
+      "error": "Invalid Parameter types"
+    });
+    return;
+  }
+
   productService( (err, data) => {
-    if(err) { }
+    if(err) {
+      res.status(503).send({
+        "status": 503,
+        "error": "Database Error"
+      });
+      return;
+    }
     res.status(200).send({
       "status": 200,
       "total": data.length,
